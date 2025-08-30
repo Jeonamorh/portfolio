d1c08cc175fbe4db67891ae24ef1ac5849ea4d23
Anmol Singh <anmol.sihra890@gmail.com>
2025-08-30 18:40:09 +0530

    photo changed

--- components/intro.tsx
+++ components/intro.tsx
@@ -28,7 +28,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src="/wordanalytics.png"
               alt="Ricardo portrait"
               width="192"
               height="192"
@@ -59,7 +59,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I&apos;m Ricardo.</span> I&apos;m a{' '}
+        <span className="font-bold">Hello, I&apos;m Anmol.</span> I&apos;m a{' '}
         <span className="font-bold">full-stack developer</span> with{' '}
         <span className="font-bold">8 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{' '}
